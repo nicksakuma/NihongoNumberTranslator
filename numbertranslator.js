36f3280387f006195a328f518f6c08fa46ea41e4
nicksakuma <nicksakuma@gmail.com>
2020-09-21 21:17:42 +0900

    add fade

--- numbertranslator.js
+++ numbertranslator.js
@@ -3,39 +3,50 @@ var english = "English";
 var japanese = "日本語";
 var leftLanguage = english;
 //Japanese units used in translating
-var units = [[100000000,"億"],[10000,"万"],[1000,"千"],[100,"百"],[10,"十"]];
+var units = [
+  [100000000, "億"],
+  [10000, "万"],
+  [1000, "千"],
+  [100, "百"],
+  [10, "十"]
+];
 var translationType = "none";
 
 //CHANGE TRANSLATION SETTINGS
-function pressTranslationTypeButton(clicked_id){
+function pressTranslationTypeButton(clicked_id) {
   document.getElementById("onClickBtn").classList.remove("btn-light")
   document.getElementById("onTypeBtn").classList.remove("btn-light")
   document.getElementById(clicked_id).classList.add("btn-light")
   translationType = clicked_id;
   console.log(translationType)
 
-  switch(translationType){
+  switch (translationType) {
     case "onClickBtn":
-      document.getElementById("onClickDiv").classList.remove("hide");
-      document.getElementById("onTypeDiv").classList.add("hide");
-    break;
+      $(document).ready(function () {
+        $("#onClickDiv").fadeToggle(500);
+        $("#onTypeDiv").hide();
+      });
+      break;
     case "onTypeBtn":
-      document.getElementById("onTypeDiv").classList.remove("hide");
-      document.getElementById("onClickDiv").classList.add("hide");
+      $(document).ready(function () {
+        $("#onTypeDiv").fadeToggle(500);
+        $("#onClickDiv").hide();
+      });
       break;
   };
 };
 
-function changeLanguage(){
+
+function changeLanguage() {
   var y = findTranslateType()
-  var leftText = document.getElementById('leftText'+y);
-  var rightText = document.getElementById('rightText'+y);
-  if(leftText.textContent === english){
+  var leftText = document.getElementById('leftText' + y);
+  var rightText = document.getElementById('rightText' + y);
+  if (leftText.textContent === english) {
     leftText.textContent = japanese;
     rightText.textContent = english;
     leftLanguage = japanese;
     clearTextBox()
-  } else if (leftText.textContent === japanese){
+  } else if (leftText.textContent === japanese) {
     leftText.textContent = english;
     rightText.textContent = japanese;
     leftLanguage = english;
@@ -44,107 +55,108 @@ function changeLanguage(){
 };
 
 //Swap language on screen 
-$(document).ready(function(){
-  $('.languageSelect').change(function() {
-    if(this.checked != true){
+$(document).ready(function () {
+  $('.languageSelect').change(function () {
+    if (this.checked != true) {
       console.log('JP<EN');
       changeLanguage();
     } else {
       console.log('EN>JP');
       changeLanguage();
     }
-  });   
+  });
 });
 
 
 
 //CLEAR TEXTAREA
 
-function clearTextBox(){
+function clearTextBox() {
   var y = findTranslateType()
-  document.getElementById("userInput"+y).value= "";
-  document.getElementById("rightTextarea"+y).innerText= "";
+  document.getElementById("userInput" + y).value = "";
+  document.getElementById("rightTextarea" + y).innerText = "";
 };
 
 //TRANSLATION
 // Check if its ontype or onclick
 function findTranslateType() {
-  if(translationType==="onClickBtn"){
-    var y="2";
+  if (translationType === "onClickBtn") {
+    var y = "2";
     return y;
   } else {
-    var y="1";
+    var y = "1";
     return y;
-  }}
-
-//Takes userinput and translates
-function numberTranslate(){
-  var y = findTranslateType()
-  var x = document.getElementById("userInput"+y).value;
-  if(leftLanguage===english){
-    console.log("translate from english")
-    document.getElementById("rightTextarea"+y).innerHTML = engToJp(x);
-  } else {
-    console.log("translate from Japanese")
-      if(isNaN(JpToEng(x))===true){
-        document.getElementById("rightTextarea"+y).innerHTML = "Incorrect Format. Check the examples below";
-      } else{
-        console.log(JpToEng(x))
-        document.getElementById("rightTextarea"+y).innerHTML = JpToEng(x);
-      }
-    
   }
 }
-  
+
+//Takes userinput and translates
+function numberTranslate() {
+  var y = findTranslateType()
+  var x = document.getElementById("userInput" + y).value;
+  if (leftLanguage === english) {
+    console.log("translate from english")
+    document.getElementById("rightTextarea" + y).innerHTML = engToJp(x);
+  } else {
+    console.log("translate from Japanese")
+    if (isNaN(JpToEng(x)) === true) {
+      document.getElementById("rightTextarea" + y).innerHTML = "Incorrect Format. Check the examples below";
+    } else {
+      console.log(JpToEng(x))
+      document.getElementById("rightTextarea" + y).innerHTML = JpToEng(x);
+    }
+
+  }
+}
+
 
 //Eng number to JP number
-function engToJp(x){
-  var intermediateValue=x;
-  var japaneseValue="";
-  for(i=0;i<units.length;i++){
+function engToJp(x) {
+  var intermediateValue = x;
+  var japaneseValue = "";
+  for (i = 0; i < units.length; i++) {
     // console.log(units[i]);
-      if((intermediateValue/units[i][0])>=1){
-          japaneseValue += Math.floor(intermediateValue/units[i][0])+units[i][1];
-          // console.log(japaneseValue);
-          intermediateValue = (intermediateValue%units[i][0]);
-          // console.log(intermediateValue);
-      };
+    if ((intermediateValue / units[i][0]) >= 1) {
+      japaneseValue += Math.floor(intermediateValue / units[i][0]) + units[i][1];
+      // console.log(japaneseValue);
+      intermediateValue = (intermediateValue % units[i][0]);
+      // console.log(intermediateValue);
+    };
   };
-  return japaneseValue+intermediateValue;
+  return japaneseValue + intermediateValue;
 };
 
 //JP number to Eng number
-function JpToEng(x){
+function JpToEng(x) {
   var intermediateValue = x;
-  var japaneseValuePlaceholder="";
-  var westernValue=0;
-    for (i=0;i<intermediateValue.length;i++){
-      var unit = findUnit(intermediateValue.charAt(i));
-      if(unit !== null){
-        westernValue += parseInt(japaneseValuePlaceholder)*unit; 
-        japaneseValuePlaceholder = "";
-      } else { 
-        japaneseValuePlaceholder += intermediateValue.charAt(i);
-      };
+  var japaneseValuePlaceholder = "";
+  var westernValue = 0;
+  for (i = 0; i < intermediateValue.length; i++) {
+    var unit = findUnit(intermediateValue.charAt(i));
+    if (unit !== null) {
+      westernValue += parseInt(japaneseValuePlaceholder) * unit;
+      japaneseValuePlaceholder = "";
+    } else {
+      japaneseValuePlaceholder += intermediateValue.charAt(i);
     };
-    if(japaneseValuePlaceholder !== ""){
-      westernValue += parseInt(japaneseValuePlaceholder);
-    };
-    return westernValue;
   };
+  if (japaneseValuePlaceholder !== "") {
+    westernValue += parseInt(japaneseValuePlaceholder);
+  };
+  return westernValue;
+};
 //Find the japanese unit in x(string)
-function findUnit(x){
-  for (y=0;y<units.length;y++){
-    if(units[y][1]===x){
-      return units[y][0];           
+function findUnit(x) {
+  for (y = 0; y < units.length; y++) {
+    if (units[y][1] === x) {
+      return units[y][0];
     }
   };
-  return null;  
+  return null;
 };
 
 //EXAMPLE
-function showExample(){
+function showExample() {
   var y = findTranslateType()
-  var element = document.getElementById("examples"+y);
+  var element = document.getElementById("examples" + y);
   element.classList.toggle("hide");
 };
